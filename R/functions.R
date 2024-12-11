2928f8ce6aa24516d3c5e4c8b952076a81d4c167
Asbjørn Kloppenborg <asbjorn.klo@gmail.com>
2024-12-11 14:17:49 +0100

    Convert column character values to snakecase format and metabolites_to_wider

--- R/functions.R
+++ R/functions.R
@@ -1,11 +1,9 @@
-#' Title
+#' Generate descriptive statistics for table 1
 #'
-#' @param data
+#' @param data The lipidomics dataset
 #'
-#' @return
-#' @export
+#' @return A dataframe/table
 #'
-#' @examples
 descriptive_stats <- function(data) {
   data %>%
     dplyr::group_by(metabolite) %>%
@@ -23,4 +21,36 @@ plot_distributions <- function(data) {
   ggplot2::ggplot(data, ggplot2::aes(x = value)) +
     ggplot2::geom_histogram() +
     ggplot2::facet_wrap(ggplot2::vars(metabolite), scales = "free")
+}
+
+
+#' Convert a column´s charater values to snakecase format
+#'
+#' @param data The lipidomics dataset
+#' @param columns The columns you want to convert into snakecase format
+#'
+#' @return a tibble/Data Frame
+column_values_to_snake_case <-
+  function(data, columns) {
+    data %>%
+      dplyr::mutate(
+        dplyr::across({{ columns }}, snakecase::to_snake_case)
+      )
+  }
+
+
+#' Function to pivot lipidomics data from long into wide, using snakecase formated column names
+#'
+#' @param data Lipidomics
+#'
+#' @return A wide data frame with 1 obs for each participants
+
+metabolites_to_wider <- function(data) {
+  data %>%
+    tidyr::pivot_wider(
+      names_from = metabolite,
+      values_from = value,
+      values_fn = mean,
+      names_prefix = "metabolite_"
+    )
 }
