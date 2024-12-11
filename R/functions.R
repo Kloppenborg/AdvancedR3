416a208f724612d02268648c9231433deafb651e
Asbjørn Kloppenborg <asbjorn.klo@gmail.com>
2024-12-11 10:08:13 +0100

    PLot distribution of metabolites

--- R/functions.R
+++ R/functions.R
@@ -1,6 +1,26 @@
+#' Title
+#'
+#' @param data
+#'
+#' @return
+#' @export
+#'
+#' @examples
 descriptive_stats <- function(data) {
-    data %>% dplyr::group_by(metabolite) %>%
-        dplyr::summarise(dplyr::across(value, list(mean = mean, sd = sd))) %>%
-        dplyr::mutate(dplyr::across(tidyselect::where(is.numeric), ~ round(.x, digits = 1)))
+  data %>%
+    dplyr::group_by(metabolite) %>%
+    dplyr::summarise(dplyr::across(value, list(mean = mean, sd = sd))) %>%
+    dplyr::mutate(dplyr::across(tidyselect::where(is.numeric), ~ round(.x, digits = 1)))
 }
 
+#' Plot for basic distribution of metabolites
+#'
+#' @param data The lipidomics dataset
+#'
+#' @return A Histogram with the GGplot2 package
+#'
+plot_distribution <- function(data) {
+  ggplot2::ggplot(data, aes(x = value)) +
+    ggplot2::geom_histogram() +
+    ggplot2::facet_wrap(ggplot2::vars(metabolite), scales = "free")
+}
